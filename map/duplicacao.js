4b2777396261b2fe49d2eee5560039298ef891d1
Pedro <pedroarmiliato2006@gmail.com>
2025-07-23 16:16:30 -0300

    add files

---
+++ map/duplicacao.js
@@ -0,0 +1,9 @@
+import readline from "readline";
+import { stdin as input, stdout as output } from "process";
+
+const rl = readline.createInterface({ input, output });
+
+const array = [1, 2, 3, 4, 5];
+console.log(array);
+
+rl.close();
